ccf0fe91db89cc600ab5201b7ed61fead4621ef7
Mike Machado <mike@machadolab.com>
2018-03-10 10:49:38 -0800

    app setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,86 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require rails-ujs
+//= require intercooler-js/src/intercooler
 //= require_tree .
+
+
+
+var AuraApp = (function() {
+
+    return {
+
+        showInfo: function (val) {
+            $.notify({icon: 'fa fa-info-circle',
+                message: val}, {
+                offset: {x: 10, y: 65},
+                animate: {
+                    enter: 'animated fadeIn',
+                    exit: 'animated fadeOut'
+                }
+            });
+        },
+
+        showError: function (val) {
+            $.notify({icon: "fa fa-exclamation-circle",
+                message: val }, {
+                type: 'danger', offset: {x: 10, y: 65}, animate: {
+                    enter: 'animated fadeIn',
+                    exit: 'animated fadeOut'
+                }
+            });
+        },
+
+        checkForNewTab: function (evt, elt) {
+            if (elt.is('[ic-get-from]') && elt.closest('[ic-push-url]').length > 0) {
+                if (evt.metaKey || evt.ctrlKey) {
+                    window.open(elt.attr('ic-get-from'));
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    };
+})();
+
+
+Intercooler.ready(function(elt) {
+
+    var sortable = elt.find('.sortable');
+    if (sortable[0]) {
+        Sortable.create(sortable[0]);
+    }
+
+    $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="popover"]').popover();
+
+});
+
+
+$(function() {
+
+    $('body').on("beforeSend.ic", function(evt, elt, data, textStatus, xhr, requestId){
+        if(Intercooler.closestAttrValue(elt, 'aura-progress-bar') == "true") {
+            $("#ajax-progress").find('div.progress-bar').remove();
+            $("#ajax-progress").html('<div id="req-' + requestId  + '-indicator" class="progress-bar" style="width: 0"></div>');
+            setTimeout(function(){
+                $("#req-" + requestId  + "-indicator").css("width", "20%");
+            }, 80);
+        }
+    }).on("complete.ic", function(evt, elt, data, textStatus, xhr, requestId){
+        var indicator = $("#req-" + requestId + "-indicator");
+        if(indicator.length > 0) {
+            indicator.css("width", "100%");
+            setTimeout(function(){
+                $("#req-" + requestId  + "-indicator").remove();
+            }, 600);
+        }
+    });
+
+    // AJAX alert events
+    $('body').on('aura.flash.notice', function(evt, val){
+        AuraApp.showInfo(val);
+    }).on('aura.flash.alert', function(evt, val){
+        AuraApp.showError(val);
+    });
+
+});
